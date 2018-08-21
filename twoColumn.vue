0f28049694030e613919fb0437bbed16f144bbf3
王英琦 <wangyingqi@judata.net>
2018-08-21 16:12:08 +0800

    twoColumn

---
+++ twoColumn.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div>
+        <div>
+            <div class="list">
+                <ul >
+                    <slot></slot>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+/* eslint-disable */
+    .list ul{
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>
